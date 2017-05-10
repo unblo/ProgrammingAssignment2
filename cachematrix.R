eeee40740c649cf309506095fb377d59396e6c81
Jacob LaChance <thedude@Jacobs-MacBook-Pro.local>
2017-05-09 23:18:39 -0400

    implemented first function

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <- NULL
+        set <- function(y) {
+                x <<- y
+                inverse <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(matrix) inverse <<- matrix
+        getInverse <- function() inverse
+        list(set = set, get = get,
+                setInverse = setInverse,
+                getInverse = getInverse)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
